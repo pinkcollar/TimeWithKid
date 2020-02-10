b0a0f55718c43c03da4e13cc2e11eb177a6d5385
Olga <saveleva.olya@gmail.com>
2020-02-10 11:09:54 -0800

    CustomView calss added to dispalay the list view

--- app/src/main/java/com/example/timewithkid/MainActivity.java
+++ app/src/main/java/com/example/timewithkid/MainActivity.java
@@ -4,24 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+    //ListView listView;
+    ListView mlistView;
+    ArrayList<Integer> images  = new ArrayList<>();
+     ;
+
+    ArrayList<String> taskNames = new ArrayList<>();
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-
-        ArrayList<String> arrayList = new ArrayList<>();
-
+        mlistView = findViewById(R.id.listView);
+        taskNames.add("CLOSED CONES");
+        taskNames.add("ICE");
+        taskNames.add("ONE+MORE");
+        images.add(R.drawable.pinecone);
+        images.add(R.drawable.pinecone);
+        images.add(R.drawable.pinecone);
+        images.add(R.drawable.pinecone);
         arrayList.add("CLOSED CONES\n" +
                 "materials: " +
                 "two pinecones from a white pine or hemlock pine" +
@@ -49,16 +66,43 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add("kjgkfj");
         arrayList.add("kjgkfj");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        CustomAdaptor customAdaptor = new CustomAdaptor();
+        mlistView.setAdapter(customAdaptor);
 
-        listView.setAdapter(arrayAdapter);
+        //ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //listView.setAdapter(arrayAdapter);
 
-            }
-        });
+
+
+    }
+
+    class CustomAdaptor extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return taskNames.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return taskNames.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView mImageView = view.findViewById(R.id.imageView);
+            TextView mTextView = view.findViewById(R.id.textView);
+            mImageView.setImageResource(images.get(position));
+            mTextView.setText(taskNames.get(position));
+            return view;
+        }
     }
 
 
