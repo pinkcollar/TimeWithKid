0a4993408e20dd702b2083af00283e11be856411
Olgs <saveleva.olya@gmail.com>
2020-02-16 00:32:56 -0800

    few tasks added

--- app/src/main/java/com/example/timewithkid/ActivityHelper.java
+++ app/src/main/java/com/example/timewithkid/ActivityHelper.java
@@ -6,26 +6,42 @@ public class ActivityHelper {
     private ArrayList<Activity> activities = new ArrayList<>();
 
 
-    public ArrayList<Activity> getActivities() {
-        return activities;
-    }
-
     public void addActivities() {
         activities.add( new Activity(R.drawable.pineconeclosedopen, R.drawable.pineconepreschoolsciencexperiment,"CLOSED CONES",
                 "materials:\n" +
-                "- two pinecones from a white pine or hemlock pine\n" +
-                "- bowl of tap water\n" +
-                "- timer",
+                        "- two pinecones from a white pine or hemlock pine\n" +
+                        "- bowl of tap water\n" +
+                        "- timer",
                 "PROCEDURE:\n1. Place one pinecode in the tap water for 10 minutes\n2. Keep one dry.\n What happens to the wet pinecone in comparison" +
-                " to the dry one?\n" +
-                "3. Let the pinecone dry out and inspect it again.",
+                        " to the dry one?\n" +
+                        "3. Let the pinecone dry out and inspect it again.",
                 "EXPLANATION:\nAfter few minutes, the wet pinecone's scales started to close. Pinecones rely on the force of wind to blow " +
                         "their seed to a spot where they can grow. Therefore, the seeds must be light and dry as possible. During rainstorms, the pinecones act as little umbrellas" +
                         "to keep the seeds dry. When the pinecone dries, it opens again. "));
 
+        activities.add( new Activity(R.drawable.feather, R.drawable.feathersbarbules,"DUCK FEATHERS", "materials:\n" +
+                "- eyedropper\n" +
+                "- tap water\n" +
+                "- goose or duck feather\n" +
+                "- magnifying lens\n", "PROCEDURE:\n1. Fill eyedropper with tap water.\n2. Place a few drops of water on the feather.\n Use the magnifying lens to observe the feather." +
+                "What happens to ther water drops?"
+                , "EXPLANATION:\nFeathers keep birds warm, and they enable them to fly. Each feather has tiny hairlike sections called barbs. Each barb has many tiny hooked barbules, " +
+                "that connect to other barbules on either side like teeth of a zipper. The barbs are covered with an oily substance, made in duck's skin, that keeps water away from the duck." +
+                "When you put the water from the eyedropper on the feather, the water just rolled off."));
+        activities.add( new Activity(R.drawable.seed, R.drawable.seedembryo,"SEED EMBRYO", "materials:\n" +
+                "- lima bean\n" +
+                "- plastic cup\n" +
+                "- tap water\n" +
+                "- magnifying lens\n" +
+                "- adult helper\n" +
+                "- knife", "PROCEDURE:\n1. Place the lima bean in the plastic cup\n2. Add enough tap water to cover the bean.\n3. Let the bean soak for about 5 minutes.\n" +
+                "4. Have your adult helper cut the bean open lengthwise. Examine the inside with magnifying lens. What do you see?",
+                "EXPLANATION:\nInside every seed is a miniature plant, or embryo. Seeds contain enough food for embryo to grow until it can make" +
+                        " its own food. Seeds have three important parts. The outer skin that protects the seed is called the seed coat. The food supply inside the seed is called the endosperm." +
+                        " The cotyledons are the first leaves the plant produces."));
         activities.add( new Activity(R.drawable.clouds, R.drawable.cloudstypes,"CLOUD PREDICTIONS",
                 "materials:\n" +
-                "- clouds\n",
+                        "- clouds\n",
                 "What Do Different Types of Clouds Mean?\n\n" +
                         "PROCEDURE:\n1. Lie on your back and observe the clouds\n2. Read the descriptions below and determine what types of clouds are in the sky.\n " +
                         "Study the clouds and determine what is going on in the atmosphere",
@@ -41,19 +57,125 @@ public class ActivityHelper {
                 "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
                 "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
                 "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
-                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. "));
-        activities.add( new Activity(R.drawable.pinecone, R.drawable.pinecone,"CLOSED CONES", "materials:\n" +
-                "- two pinecones from a white pine or hemlock pine\n" +
-                "- bowl of tap water\n" +
-                "- timer", "PROCEDURE:\n1. Place one pinecode in the tap water for 10 minutes\n2. Keep one dry.\n What happens to the wet pinecone in comparison" +
-                " to the dry one?\n" +
-                "3. Let the pinecone dry out and inspect it again.", "EXPLANATION:\nAfter few minutes, the wet pinecone's scales started to close."));
-        activities.add( new Activity(R.drawable.pinecone, R.drawable.pinecone,"CLOSED CONES", "materials:\n" +
-                "- two pinecones from a white pine or hemlock pine\n" +
-                "- bowl of tap water\n" +
-                "- timer", "PROCEDURE:\n1. Place one pinecode in the tap water for 10 minutes\n2. Keep one dry.\n What happens to the wet pinecone in comparison" +
-                " to the dry one?\n" +
-                "3. Let the pinecone dry out and inspect it again.", "EXPLANATION:\nAfter few minutes, the wet pinecone's scales started to close."));
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.soilinbag, R.drawable.soilonwindow,"WATER CYCLE", "materials:\n" +
+                "- soil\n" +
+                "- plastic zip bag\n" +
+                "- tap water\n" +
+                "- masking tape", "PROCEDURE:\n1. Place the soil in the bottom of the plastic bag.\n2. Sprinkle just enough water on the soil to dampen it.\n" +
+                "3. Close the bag so it is tight and tape it to a sunny window.\n" +
+                "4. Observe the bag for the next few minutes. What happens in the bag?",
+                "EXPLANATION:\nThe water in o ur environment moves in a cycle. When it rains, water falls and soaks into the earth or runs into waterways. " +
+                "When water on the earth is heated, it turns into water vapor and is carried around in the air. The water partially evaporates. This vapor condenses in atmosphere and forms clouds, " +
+                        "witch produce rain. " +
+                "As the sun warmed the soil in the bag, you saw water droplets form at the top pf the bag. When enough water collected at the top  of the bag, " +
+                        "it became heavy and fell back to the soil like rain." ));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+        activities.add( new Activity(R.drawable.egg, R.drawable.egginside,"INVESTIGATING EGGS", "materials:\n" +
+                "- chicken egg\n" +
+                "- magnifying lens\n" +
+                "- bowl\n" +
+                "- toothpick", "PROCEDURE:\n1. If the egg was in the refrigerator, let it sit out for a while to warm.\n2. Exemine the outside og the egg under the magnifying lens.\n" +
+                "3. Slowly crack the egg open over a bowl and examine the inside of the shell under magnifying lens.\n" +
+                "4. Study the inside of the egg. Notice the membrane inside the shell.\n" +
+                "5. Examine the yolk. What is the thin membrane around the yolk holding it in a round shape?", "EXPLANATION:\nA chicken egg is a single cell that contains its own food. " +
+                "The white or brown hard surface, the shell, is part of protective membrane that surrounds the inside if the egg. When you cracked the egg open, you sa the membrane " +
+                "that surrounds the inside of the egg. The yellow center of the egg is the yolk, which is the food source for thr developing chick. The yolk in place " +
+                "on top of the growing chick. The white part of the egg is called the albumen. The albumen contains water reserved for the chick and holds the yolk."));
+    }
+
+    public ArrayList<Activity> getActivities() {
+        return activities;
     }
 
 
